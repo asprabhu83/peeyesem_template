a992a81f787cf56ba55c0aeaa06040f856b88122
ranjithgee <ranjithgee968@gmail.com>
2021-09-29 11:18:16 +0530

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -103,6 +103,15 @@
 
     <section class="pt-20 cars_product_section"  style="background: #e6eaef;">
         <div class="text-center font-bold text-3xl text-uppercase car_sec_head" >Choose Your Dream Hyundai Car<hr class="h-1 w-2/12 my-2 mx-auto bg-black" style="background-color:black!important;" /></div>
+        <div class="mt-14 category_sec">
+            <div class="container">
+                <div class="flex items-center justify-center">
+                    <div class="mx-3 px-2 category_item font-semibold text-lg cursor-pointer"  v-for="(item, index) in carCategory" :class="categoryIndex == index ? 'active': ''" @click="categoryIndex = index, filterItems(item)" :key="index">
+                        {{item}}
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="card_box  flex flex-wrap mt-16 mx-auto">
             <div class="car_box_item" v-for="car in staticcars" :key="car.id">
                 <a :href="'/cars/?id='+ car.id" class="text-center block car_image_box cursor-pointer" >
@@ -499,75 +508,180 @@ export default {
                 id:1,
                 name:'Hyundai Tuscon',
                 image:'car1.webp',
+                category:'Hatchback',
                 price:'4,67,490'
             },
             {
                 id:2,
                 name:'Hyundai Verna',
                 image:'car3.jpg',
+                category:'Hatchback',
                 price:'8,19,900'
             },
             {
                 id:3,
                 name:'Hyundai Aura',
                 image:'car2.webp',
+                category:'Hatchback',
                 price:'22,30,000'
             },
             {
                 id:4,
                 name:'Hyundai Venue',
                 image:'car4.webp',
+                category:'Hatchback',
                 price:'8,16,500'
             },
             {
                 id:5,
                 name:'Hyundai Kona',
                 image:'car6.jpg',
+                category:'Sedan',
                 price:'9,99,900'
             },
             {
                 id:6,
                 name:'Hyundai Alcazar',
                 image:'car5.jpg',
+                category:'Sedan',
                 price:'16,53,300'
             },
             {
                 id:7,
                 name:'Hyundai All New I20',
                 image:'car7.jpg',
+                category:'Sedan',
                 price:'8,19,900'
             },
             {
                 id:8,
                 name:'Hyundai Santro',
                 image:'car8.jpg',
+                category:'SUV',
                 price:'4,67,490'
             },
             {
                 id:9,
                 name:'GRAND i10 NIOS',
                 image:'car9.webp',
+                category:'SUV',
                 price:'8,14,900'
             },
             {
                 id:10,
                 name:'Hyundai i20 N Line',
                 image:'car10.webp',
+                category:'SUV',
                 price:'7,31,900'
             },
             {
                 id:11,
                 name:'Hyundai ELANTRA',
                 image:'car11.webp',
+                category:'SUV',
                 price:'10,68,000'
             },
             {
                 id:12,
                 name:'Hyundai CRETA',
                 image:'car12.jpg',
+                category:'SUV',
                 price:'17,00,000'
             }
         ],
+
+        originalcars:[
+            {
+                id:1,
+                name:'Hyundai Tuscon',
+                image:'car1.webp',
+                category:'Hatchback',
+                price:'4,67,490'
+            },
+            {
+                id:2,
+                name:'Hyundai Verna',
+                image:'car3.jpg',
+                category:'Hatchback',
+                price:'8,19,900'
+            },
+            {
+                id:3,
+                name:'Hyundai Aura',
+                image:'car2.webp',
+                category:'Hatchback',
+                price:'22,30,000'
+            },
+            {
+                id:4,
+                name:'Hyundai Venue',
+                image:'car4.webp',
+                category:'Hatchback',
+                price:'8,16,500'
+            },
+            {
+                id:5,
+                name:'Hyundai Kona',
+                image:'car6.jpg',
+                category:'Sedan',
+                price:'9,99,900'
+            },
+            {
+                id:6,
+                name:'Hyundai Alcazar',
+                image:'car5.jpg',
+                category:'Sedan',
+                price:'16,53,300'
+            },
+            {
+                id:7,
+                name:'Hyundai All New I20',
+                image:'car7.jpg',
+                category:'Sedan',
+                price:'8,19,900'
+            },
+            {
+                id:8,
+                name:'Hyundai Santro',
+                image:'car8.jpg',
+                category:'SUV',
+                price:'4,67,490'
+            },
+            {
+                id:9,
+                name:'GRAND i10 NIOS',
+                image:'car9.webp',
+                category:'SUV',
+                price:'8,14,900'
+            },
+            {
+                id:10,
+                name:'Hyundai i20 N Line',
+                image:'car10.webp',
+                category:'SUV',
+                price:'7,31,900'
+            },
+            {
+                id:11,
+                name:'Hyundai ELANTRA',
+                image:'car11.webp',
+                category:'SUV',
+                price:'10,68,000'
+            },
+            {
+                id:12,
+                name:'Hyundai CRETA',
+                image:'car12.jpg',
+                category:'SUV',
+                price:'17,00,000'
+            }
+        ],
+
+        shuffledCars:null,
+
+        carCategory:null,
+
+        categoryIndex:0,
 
         // Todays Slider options 
         swiperOption: {
@@ -583,7 +697,7 @@ export default {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev'
                 },
-                autoplay: false,
+                autoplay: true,
             },
 
       }
@@ -599,6 +713,7 @@ export default {
 
         this.productsArray()
         this.GetCars()
+        this.categoryNames()
 
         const obj1 = document.getElementById("value1");
         const obj2 = document.getElementById("value2");
@@ -619,6 +734,19 @@ export default {
         window.removeEventListener('scroll', this.handleScroll);
     },
     methods: {
+        filterItems(name){
+          var newItems =  this.originalcars.filter((item)=> item.category == name);
+          this.staticcars = newItems;
+          if(name == 'ALL'){
+              this.staticcars = this.originalcars
+          }
+          console.log(this.staticcars)
+        },
+        categoryNames(){
+           var category = ['ALL',...new Set(this.staticcars.map((item)=>{return item.category}))]
+           this.carCategory = category;
+           
+        },
         handleScroll(){
             var btn = document.querySelector('.back_to_top')
             if(pageYOffset > 260){
@@ -703,6 +831,11 @@ export default {
 }
 </script>
 <style scoped>
+
+.category_item.active{
+    border-bottom:2px solid #002c5f;
+    color:#002c5f;
+}
 .form_modal_box{
     height: 100vh;
     width: 100%;
