5c2dc40dffdf27a3398ddbf8b3ffe648c36028e2
ranjithgee <ranjithgee968@gmail.com>
2021-10-25 16:51:25 +0530

    changes

--- pages/accessories.vue
+++ pages/accessories.vue
@@ -1,21 +1,89 @@
 <template>
   <div class="accessories_page">
-     <div class="main_poster_image">
-        <img :src="require('@/assets/img/cars/car15.webp')" alt="img" style="width:100%;" />
+     <div class="acc_head_title mt-16 mb-8">
+         <div class="text-center title">Accessories</div>
      </div>
-     <div class="acc_head_title my-16">
-         <div class="text-center title">Browse by Car Type</div>
-     </div>
-     <div class="car_accessories_sec">
-         <div class="car_item_box" >
-             <div class="car_item" v-for="car in originalcars" :key="car.id">
-                 <a :href="'/genuine-accessories/?id='+ car.id" class="text-center block cursor-pointer" >
-                    <img :src="require('@/assets/img/cars/static/'+ car.image)" alt="" >
-                </a>
-                <div class="car_item_title">{{car.name}}</div>
-             </div>
-         </div>
-     </div>
+     <div class="form_sec">
+          <div class="step step1" :class="form_tab_index == 0 ? 'active' : ''" >
+               <div class="title">Personal Details</div>
+            <form >
+                <div class="mb-4 mt-4">
+                        <input
+                        class="
+                            shadow-md
+                            appearance-none
+                            border
+                            rounded
+                            w-full
+                            py-2
+                            px-3
+                            text-gray-700
+                            leading-tight
+                            focus:outline-none
+                            focus:shadow-outline
+                        "
+                        id="name"
+                        type="text"
+                        placeholder="Full Name"
+                        v-model="name"
+                        />
+                    </div>
+                    <div class="mb-4 ">
+                        <input
+                        class="
+                            shadow-md
+                            appearance-none
+                            border
+                            rounded
+                            w-full
+                            py-2
+                            px-3
+                            text-gray-700
+                            leading-tight
+                            focus:outline-none
+                            focus:shadow-outline
+                        "
+                        id="email"
+                        type="email"
+                        placeholder="Email Id"
+                        v-model="email"
+                        />
+                    </div>
+                    <div class="mb-4 ">
+                        <input
+                        class="
+                            shadow-md
+                            appearance-none
+                            border
+                            rounded
+                            w-full
+                            py-2
+                            px-3
+                            text-gray-700
+                            leading-tight
+                            focus:outline-none
+                            focus:shadow-outline
+                        "
+                        id="mobile"
+                        type="text"
+                        placeholder="Mobile Number"
+                        v-model="mobile"
+                        />
+                    </div>
+                    <div class="mb-6">
+                        <div class="checkbox_sec">
+                            <label class="inline-flex items-center">
+                                <input type="checkbox" class="form-checkbox">
+                                <span class="ml-2 cursor-pointer">I have read & understood the disclaimer</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="btn_box">
+                        <button type="button">Submit</button>
+                    </div>
+            </form>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -102,6 +170,10 @@ export default {
                 price:'17,00,000'
             }
             ],
+            name:'',
+            email:'',
+            mobile:'',
+            form_tab_index:0
         }
     }
 }
@@ -112,33 +184,78 @@ export default {
     font-size: 28px;
     font-weight: 600;
 }
-.car_accessories_sec .car_item_box{
-    width: 80%;
-    margin:40px auto;
+.heading{
+    font-weight: 600;
+    font-size: 25px;
+}
+.explain{
+    font-size: 16px;
+    width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+}
+.btn_box button{
+    padding: 4px 20px;
+    background: #002c5f;
+    color: white;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    border-radius: 4px;
+}
+.form_sec{
+    width: 60%;
+    margin: 30px auto;
     display: flex;
-    flex-wrap: wrap;
 }
-.car_accessories_sec .car_item_box .car_item{
-    width: 22%;
-    border-radius: 5px;
-    overflow: hidden;
-    margin: 20px;
-    position: relative;
-}
-.car_item_box .car_item a{
-    min-height: 220px;
-    max-height: 220px;
-}
-.car_item a img{
-    width: 100%;
-    min-height: 220px;
-    max-height: 220px;
-    max-width: 100%;
+.form_sec .image_sec{
+    width: 49%;
+    height: 370px;
     object-fit: cover;
-    transition: .4s ease-in-out;
+    object-position: left;
 }
-.car_item a img:hover{
-    transform: scale(1.1);
+.image_sec img{
+    height: 100%;
+}
+.form_sec .step{
+    width: 49%;
+    padding: 0 40px;
+    margin: 30px auto;
+    display: none;
+}
+.form_sec .step.active{
+    display: block;
+}
+.form_sec .step .title{
+    font-weight: 600;
+    font-size: 19px;
+}
+.active_sec{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.active_sec div{
+    height: 10px;
+    width: 10px;
+    background: lightgray;
+    margin: 10px;
+}
+.active_sec div.active{
+    background: #002c5f;
+    height: 11px;
+    width: 11px;
+}
+.step2 .btn_box{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.checkbox_sec span{
+    font-size: 15px;
+}
+.checkbox_sec{
+    padding-left:4px;
 }
 .car_accessories_sec .car_item_title{
     color: white;
