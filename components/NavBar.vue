086c326ca33be5f84b4729493a090d0c6251a6b2
ranjithgee <ranjithgee968@gmail.com>
2021-11-18 15:41:23 +0530

    changes

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -35,15 +35,33 @@
                       <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
                       <nuxt-link to="/psm-admin/user-list" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-1">Manage Users</nuxt-link>
                       <nuxt-link to="/psm-admin/user-role-list" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Manage UserRole</nuxt-link>
+                      <nuxt-link to="/psm-admin/add-page" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Add Pages</nuxt-link>
+                      <nuxt-link to="/psm-admin/settings" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Edit Settings</nuxt-link>
+                      <nuxt-link to="/psm-admin/forms-list" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Forms List</nuxt-link>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item ml-3">
+                <div class="relative inline-block text-left">
+                  <div>
+                    <button type="button" @click="drpdwn2 = true"  class="drpdwn2 outline-none border-none inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-transparent text-sm font-medium text-gray-700 " id="menu-button" aria-expanded="true" aria-haspopup="true">
+                      Manage Cars
+                      <!-- Heroicon name: solid/chevron-down -->
+                      <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                      </svg>
+                    </button>
+                  </div>
+                  <div v-if="drpdwn2 === true" @mouseleave="drpdwn2 = false" class="origin-top-right absolute -right-26 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                    <div class="" role="none">
+                      <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
                       <nuxt-link to="/psm-admin/add-cars" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Add Cars</nuxt-link>
                       <nuxt-link to="/psm-admin/manage-cars" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Manage Cars</nuxt-link>
                       <nuxt-link to="/psm-admin/add-menu" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Add Menus</nuxt-link>
                       <nuxt-link to="/psm-admin/testimonial-list" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Add Testimonials</nuxt-link>
-                      <nuxt-link to="/psm-admin/add-page" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Add Pages</nuxt-link>
-                      <nuxt-link to="/psm-admin/settings" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Edit Settings</nuxt-link>
                       <nuxt-link to="/psm-admin/manage-used-cars" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Manage Used Cars</nuxt-link>
                       <nuxt-link to="/psm-admin/blog-list" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Add Blogs</nuxt-link>
-                      <nuxt-link to="/psm-admin/forms-list" class="text-gray-700 block px-4 py-3 text-sm  dropdown_menu_item" role="menuitem" tabindex="-1" id="menu-item-2">Forms List</nuxt-link>
                     </div>
                   </div>
                 </div>
@@ -63,6 +81,7 @@ export default {
   data () {
     return {
       drpdwn: false,
+      drpdwn2: false,
       permission: '',
       type: '',
       projectPermission: ''
