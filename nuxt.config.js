8a201339635c68e3b425b337ebeb7079b2b6cb48
ranjithgee <ranjithgee968@gmail.com>
2022-01-06 12:26:02 +0530

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon5.ico' },
+      { rel:'stylesheet', href:'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'}
     ]
   },
 
